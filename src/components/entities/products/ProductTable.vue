30884da53769a77968ddb71ed87f6ec0b7f08848
dmlen <dmlen@rollncode.com>
2023-11-29 15:49:54 +0200

    [TASK03] feat: add slot for "tags" column to handle tag name
    – Tag name passing is done via slot. It is done so to prevent "EntityTable" component taking the responsibility of providing a name for each Tag by Tag id.

--- src/components/entities/products/ProductTable.vue
+++ src/components/entities/products/ProductTable.vue
@@ -1,11 +1,17 @@
 <template>
-  <EntityTable :entity-type="entityType" :headers="state.headers" />
+  <EntityTable :entity-type="entityType" :headers="state.headers">
+    <template #tagChipContent="{tagId}">
+      {{ mapTagIdToTagName.get(tagId) ?? '?' }}
+    </template>
+  </EntityTable>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
+import { computed, defineComponent, onBeforeMount, reactive, ref, Ref } from 'vue';
+import { useStore } from 'vuex';
 import { EntityType } from '../../../store/entityModules/types';
 import EntityTable from '../../common/EntityTable.vue';
+import { Tag } from '../../../models/entities/Tag';
 
 const Component = defineComponent({
   name: 'ProductTable',
@@ -15,20 +21,39 @@ const Component = defineComponent({
   },
 
   setup() {
+    const store = useStore();
     const entityType = EntityType.PRODUCT;
+
+    let listTag: Ref<Tag[]> = ref([]);
+    onBeforeMount(async () => {
+      listTag.value = await store.dispatch(
+        'tagsModule/fetchAllItems',
+        undefined,
+      );
+    });
 
     const state = reactive({
       headers: [
         { title: 'Name', key: 'name', sortable: true, searchable: true },
         { title: 'Category ID', key: 'categoryId', sortable: true },
         { title: 'Description', key: 'description', sortable: false },
+        { title: 'Tags', key: 'tags', sortable: false },
         { title: 'Details', key: 'actions', sortable: false },
       ],
+    });
+
+    const mapTagIdToTagName = computed(() => {
+      const map = new Map();
+      listTag.value.forEach((tag) => {
+        map.set(tag.id, tag.name);
+      });
+      return map;
     });
 
     return {
       entityType,
       state,
+      mapTagIdToTagName,
     };
   },
 });
