4d43ee712a88bccbed7b2631e1474c2d67032879
dmlen <dmlen@rollncode.com>
2023-11-29 15:50:45 +0200

    [TASK03] feat: create "TagTable" component
    – Is used to list Tag entities

---
+++ src/components/entities/tags/TagTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <EntityTable :entity-type="entityType" :headers="state.headers" />
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+import { EntityType } from '../../../store/entityModules/types';
+import EntityTable from '../../common/EntityTable.vue';
+
+const Component = defineComponent({
+  name: 'TagTable',
+
+  components: {
+    EntityTable,
+  },
+
+  setup() {
+    const entityType = EntityType.TAG;
+
+    const state = reactive({
+      headers: [
+        { title: 'Name', key: 'name', searchable: true },
+        { title: 'Details', key: 'actions', align: 'end', sortable: false },
+      ],
+    });
+
+    return {
+      entityType,
+      state,
+    };
+  },
+});
+export default Component;
+</script>
+
+<style lang="scss" scoped></style>
